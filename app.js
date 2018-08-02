67650f0b0c1d03a2ace37b859e7e4f4773295750
Idrogo Mora Edixon Jhoan <eidrog01@cantv.com.ve>
2018-08-02 15:59:06 -0400

    agregando nuesvos cambios para la vista

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
-var express = require('express');
+var express = require('express'),
+	path = require('path'),
+	bodyParser = require('body-parser'),
+	cons = require('consolidate'),
+	dust = require('dustjs-helpers'),
+	request = require('request'),
+	axios=require('axios');
+
+
+
 
 var app = express();
+app.set('views', __dirname + '/views');
 
 app.get('/', function(req, res) {
-  res.send('hello, Edixon');
+  res.render('index')
 });
 
 app.listen(process.env.PORT || 5000);
