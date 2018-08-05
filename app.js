c5541ad4092383ee7e44a2ce4e8a3df996556588
Edixon Idrogo <idrogo.edixon@gmail.com>
2018-08-05 12:22:19 -0400

    test image

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.get('/', function(req, res) {
 	let axios = require('axios');
 	let cheerio = require('cheerio');
 
-	let base_url = 'http://www.bca.co.id/id/Individu/Sarana/Kurs-dan-Suku-Bunga/Kurs-dan-Kalkulator';
+/*	let base_url = 'http://www.bca.co.id/id/Individu/Sarana/Kurs-dan-Suku-Bunga/Kurs-dan-Kalkulator';
 
 	axios.get(base_url).then( (response) => {
 	  let $ = cheerio.load(response.data);
@@ -49,8 +49,35 @@ app.get('/', function(req, res) {
 	.then ( (kurs) => {
 	  res.json(kurs);
 	});
+*/
 
+	let base_url = 'https://www.amazon.com/gp/registry/wishlist/1A7GB9IL1UAK2';
 
+	axios.get(base_url).then( (response) => {
+	  let $ = cheerio.load(response.data);
+	  let kurs = [];
+	  $('tr', '.text-right').each( (i, elm) => {
+	    kurs.push( {
+	      currency: $(elm).children().first().text(),
+	      erate: {
+	        sell: $(elm).children().eq(1).first().text(),
+	        buy: $(elm).children().eq(2).first().text()
+	      },
+	      tt: {
+	        sell: $(elm).children().eq(3).first().text(),
+	        buy: $(elm).children().eq(4).first().text()
+	      },
+	      notes: {
+	        sell: $(elm).children().eq(5).first().text(),
+	        buy: $(elm).children().eq(6).first().text()
+	      }
+	    });
+	  });
+	  return(kurs);
+	})
+	.then ( (kurs) => {
+	  res.json(kurs);
+	});
 
 
 
