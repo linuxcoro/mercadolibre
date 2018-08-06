3069f59ef88820cd6cc6b0e1c6ff9760339cfbd2
Idrogo Mora Edixon Jhoan <eidrog01@cantv.com.ve>
2018-08-06 16:28:49 -0400

    test

--- app.js
+++ app.js
@@ -21,18 +21,29 @@ app.get('/', function(req, res) {
     url = 'https://www.amazon.com/registry/wishlist/1A7GB9IL1UAK2/';
     request(url, function(error, response, html){
         if(!error){
-            var $ = cheerio.load(html);
 
 /*
+            var $ = cheerio.load(html);
             var imagen = $('.g-itemImage img').attr("src");
            	res.render('index', {'json': imagen });
 */           	
 
-			$('.g-itemImage img').each(function(i, elem) {
-			  imagen[i] = $(this).text();
-			});
-           	res.render('index', {'json': imagen });
-
+	  let $ = cheerio.load(response.data);
+	  let kurs = [];
+	  $('.g-itemImage img').each( (i, elm) => {
+	    kurs.push( {
+	      currency: $(elm).children().first().text(),
+	      erate: {
+	        sell: $(elm).children().eq(0).first().text(),
+	        buy: $(elm).children().eq(2).first().text()
+	      }
+	    });
+	  });
+	  return(kurs);
+	})
+	.then ( (kurs) => {
+	  res.json(kurs);
+	});
 
 
         }
