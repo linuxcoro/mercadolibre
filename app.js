4948316c4b2db92615071626f5e077cab81b933f
Idrogo Mora Edixon Jhoan <eidrog01@cantv.com.ve>
2018-08-08 16:31:49 -0400

    creando el arreglo de lista de deseos

--- app.js
+++ app.js
@@ -1,33 +1,48 @@
 var path = require('path'),
-	bodyParser = require('body-parser'),
-	cons = require('consolidate'),
-	dust = require('dustjs-helpers'),
-	request = require('request'),
-	axios=require('axios'),
-	express = require('express');
+    bodyParser = require('body-parser'),
+    cons = require('consolidate'),
+    dust = require('dustjs-helpers'),
+    request = require('request'),
+    axios=require('axios'),
+    express = require('express');
 
 var cheerio = require('cheerio');
 
 var app = express();
 
-	app.engine('dust', cons.dust);
-	app.set('view engine', 'dust');
-	app.set('views', __dirname + '/views');
-
-
+    app.engine('dust', cons.dust);
+    app.set('view engine', 'dust');
+    app.set('views', __dirname + '/views');
 
 
 app.get('/', function(req, res) {
     url = 'https://www.amazon.com/registry/wishlist/1A7GB9IL1UAK2/';
     request(url, function(error, response, html){
         if(!error){
-			var $ = cheerio.load(html);
-			var kurs = [];
-			$('.g-itemImage img').each(function(i, elem) {
-				kurs[i] = $(this).attr("src");
-			});
-           	res.render('index', {'json': kurs });
+            var $ = cheerio.load(html);
+            var img = [];
+            var tit = [];       
+            var pre = [];
 
+            $('.a-fixed-left-grid .a-spacing-none').find('.a-size-base a').each(function(i, elem) {
+                    tit[i] = $(this).text();
+            });
+
+            $('.a-fixed-left-grid .a-spacing-none').find('span .a-offscreen').each(function(i, elem) {
+                pre[i] = $(this).text();
+            });
+
+            $('.a-fixed-left-grid .a-spacing-none').find('.g-itemImage img').each(function(i, elem) {
+                img[i] = $(this).attr("src");
+            });
+
+
+
+
+
+            var kurs = { 'titulo': tit , 'precio': pre ,  'imagen': img }
+
+            res.json({ 'imagen': kurs });
         }
     })
 });
