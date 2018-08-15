4c72e4b28b625d7f590d53461ea4813035aaf710
Idrogo Mora Edixon Jhoan <eidrog01@cantv.com.ve>
2018-08-15 15:08:49 -0400

    conexion a postgresql por bash

---
+++ conexion_postgresql.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+set -u
+
+# Set these environmental variables to override them,
+# but they have safe defaults.
+export PGHOST=ec2-50-17-194-129.compute-1.amazonaws.com
+export PGDATABASE=ddjgevmsurduca
+export PGUSER=nkhkzwpxietfbc
+export PGPASSWORD=52140030c14ffbdd15a93968ba3e186a59acff91055d061798d7d86f7a9b21e9
+
+RUN_PSQL="psql -X --set AUTOCOMMIT=off --set ON_ERROR_STOP=on "
+${RUN_PSQL} <<SQL
+insert into medidas (nombre,precio,imagen) VALUES('2 Pack - DIRECTV IR / RF Universal Remote Control (RC66RX)',9.64,'71lK+J4v9VL._SS135_.jpg');
+SQL
--- url.py
+++ url.py
@@ -1,63 +1,21 @@
 import requests
-import pymongo
-#req = 'http://localhost:8080'
-#r = requests.get(req)
-# if r.status_code == 200:
-# 	decoded = r.json()
-# 	for x in decoded:
-# 		for n in xrange(0,len(decoded[x])):
-# 			print '----------------------------------------------'
-# 			print decoded[x][n]['hash']
-# 			print decoded[x][n]['titulo']['t']
-# 			print decoded[x][n]['precio']
+#no funciona
+#import pymongo
+req = 'https://linuxcoro.herokuapp.com/'
+r = requests.get(req)
+if r.status_code == 200:
+	decoded = r.json()
+	for x in decoded:
+		for n in xrange(0,len(decoded[x])):
+			print '----------------------------------------------'
+			print decoded[x][n]['hash']
+			print decoded[x][n]['titulo']['t']
+			print decoded[x][n]['precio']
 
-# 			url = decoded[x][n]['imagen']
-# 			filename = url.split('/')[-1]
-# 			print filename
-# 			r = requests.get(url, allow_redirects=True)
-# 			open('public/img/'+filename, 'wb').write(r.content)
-# 			print '----------------------------------------------'
-
-SEED_DATA = [
-    {
-        'decade': '1970s',
-        'artist': 'Debby Boone',
-        'song': 'You Light Up My Life',
-        'weeksAtOne': 10
-    },
-    {
-        'decade': '1980s',
-        'artist': 'Olivia Newton-John',
-        'song': 'Physical',
-        'weeksAtOne': 10
-    },
-    {
-        'decade': '1990s',
-        'artist': 'Mariah Carey',
-        'song': 'One Sweet Day',
-        'weeksAtOne': 16
-    }
-]
-
-#uri = 'mongodb://user:pass@host:port/db'
-#uri = 'mongodb://eidrogo:ne0Jahz2at@ds243798.mlab.com:43798/linuxcoro' 
-uri = "mongodb://test:test123@ds243798.mlab.com:43798/linuxcoro"
-#'mongodb://<dbuser>:<dbpassword>@ds243798.mlab.com:43798/linuxcoro'
-client = pymongo.MongoClient(uri)
-
-#db = client.get_default_database('linuxcoro')
-
-# First we'll add a few songs. Nothing is required to create the songs 
-# collection; it is created automatically when we insert.
-
-#songs = db['usuarios']
-
-# Note that the insert method can take either an array or a single dict.
-
-#songs.insert_many(SEED_DATA)
-
-db = client.get_database("linuxcoro")
-
-songs = db.usuarios
-songs.insert_one({"nombre" : "maria" , "apellido": "colina", "hijos": 1 })
+			url = decoded[x][n]['imagen']
+			filename = url.split('/')[-1]
+			print filename
+			r = requests.get(url, allow_redirects=True)
+			open('public/img/'+filename, 'wb').write(r.content)
+			print '----------------------------------------------'
 
