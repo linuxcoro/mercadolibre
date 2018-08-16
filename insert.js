dbb2c270abac20f13594ac20660f446a43e074e7
Idrogo Mora Edixon Jhoan <eidrog01@cantv.com.ve>
2018-08-16 15:59:38 -0400

    reconstruyendo srapin

--- insert.js
+++ insert.js
@@ -1,12 +1,27 @@
 /* consultar la api */
 let request = require('request')
 let url='https://linuxcoro.herokuapp.com/'
-request(url, function(error, response, html){
-    console.log(html)
+
+request(url, function(error, r, html){
+    let arr = JSON.parse(html)
+    //console.log(html)
+    if(r.statusCode==200){
+        art=arr.lista
+        for (let index = 0; index < art.length; index++) {
+            const element = art[index];
+            console.log(element.hash)
+            console.log(element.titulo.t)
+            console.log(element.titulo.detalle)
+            console.log(element.precio)
+            console.log(element.imagen)
+            console.log('---------------------------------')
+        }
+    }
 })
 
-/* insertar valores */
-/* var MongoClient = require('mongodb').MongoClient;
+
+ /* insertar valores */
+var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://test:test123@ds243798.mlab.com:43798/linuxcoro";
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
@@ -17,4 +32,4 @@ MongoClient.connect(url, function(err, db) {
     console.log("1 document inserted");
     db.close();
   });
-}); */
+});
