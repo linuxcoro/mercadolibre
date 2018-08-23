32d20627477f439ed857e4c7275f2b235a8314ec
Idrogo Mora Edixon Jhoan <eidrog01@cantv.com.ve>
2018-08-23 10:38:27 -0400

    corrigiendo precio

--- insert.js
+++ insert.js
@@ -1,8 +1,6 @@
 /* consultar la api */
 const cheerio = require('cheerio');
 const request = require('request')
-const requestPromise = require('request-promise');
-const Promise = require('bluebird');
 
 let url='https://linuxcoro.herokuapp.com/'
 
@@ -11,25 +9,6 @@ let url='https://linuxcoro.herokuapp.com/'
  var con = "mongodb://test:test123@ds243798.mlab.com:43798/linuxcoro";
 
 function detalle(str){
-/* 
-    Promise.map(urls, requestPromise) 
-    .map((htmlOnePage, index) => {
-      var det = undefined
-      const $ = cheerio.load(htmlOnePage);
-  
-      // detalles
-      $('#feature-bullets').find('.a-list-item').each(function(i,elem){
-          if($(this).text().trim()!==undefined)
-            det+=$(this).text().trim()
-      })
-      // imagenes        
-      const img = $('#landingImage').attr('data-old-hires')
-        return {det,img}
-    })    
-    .then(console.log)
-    .catch((e) => console.log('We encountered an error' + e));
-
- */     
     let url=str.des
     let detalle
     let tod=[]
@@ -46,26 +25,18 @@ function detalle(str){
             // imagenes        
             let img = $('#landingImage').attr('data-old-hires')
             tod = {str,detalle,img}
-            if(tod.detalle!=undefined){
-                console.log("hash: "+tod.str.hash)
-                console.log("titulo: "+tod.str.titulo)
-                console.log("precio: "+tod.str.precio)
-                console.log("detalle: "+tod.detalle)
-                console.log("imagen: "+tod.img)
-                
-                
+            if(tod.detalle!=undefined && tod.img!=""){
+                //MongoClient.connect(con,{ useNewUrlParser: true }, function(err, db) {
                 MongoClient.connect(con,{ useNewUrlParser: true }, function(err, db) {
-                    if (err) throw err;
                     var dbo = db.db("linuxcoro");
                     dbo.collection("articles").findOne({'hash':tod.str.hash}, function(err, result) {
-                        if (err) { }                
                         if (!result) {
                             var myobj = {
                                 hash: tod.str.hash,
                                 titulo: tod.str.titulo,
                                 detalle: tod.detalle,
-                                precio: tod.str.precio,
-                                imagen: tod.imagen
+                                precio: tod.str.precio.substr(1),
+                                imagen: tod.img
                             };
                             dbo.collection("articles").insertOne(myobj, function(err, res) {
                               if (err) throw err;
@@ -73,10 +44,12 @@ function detalle(str){
                               db.close();
                             });
                         }
+                        else{
+                            db.close();
+                            console.log('no')
+                        }
                     }) 
                 });
-    
-
 
 
 
@@ -109,33 +82,6 @@ request(url, function(error, r, html){
                 precio: element.precio
             };
 
-            /* clave de los detalles */
-
-
-                    /* 
-            MongoClient.connect(con,{ useNewUrlParser: true }, function(err, db) {
-                if (err) throw err;
-                var dbo = db.db("linuxcoro");
-                dbo.collection("articles").findOne({'hash':element.hash}, function(err, result) {
-                    if (err) { }                
-                    if (!result) {
-                        var myobj = {
-                            hash: element.hash,
-                            titulo: element.titulo.t,
-                            detalle: element.titulo.detalle,
-                            precio: element.precio,
-                            detalle: element.imagen
-                        };
-                        dbo.collection("articles").insertOne(myobj, function(err, res) {
-                          if (err) throw err;
-                          console.log("1 document inserted");
-                          db.close();
-                        });
-                    }
-                }) 
-            });
-
- */                        
             detalle(myobj)
 
         }
