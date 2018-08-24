e425d05c08a6f6d2d6a5849eb6713e33d5df26be
Idrogo Mora Edixon Jhoan <eidrog01@cantv.com.ve>
2018-08-24 17:08:02 -0400

    ingresando datos simples mongo

--- insert.js
+++ insert.js
@@ -7,43 +7,36 @@ var url='https://linuxcoro.herokuapp.com/'
 var MongoClient = require('mongodb').MongoClient;
 var con = "mongodb://test:test123@ds243798.mlab.com:43798/linuxcoro";
 
-function detalle(str){
-    var urls=str.des
-    var detalle
-    var tod=[]
-    
-    request(urls, function(error, response, html){
-        if(!error){
-            var $ = cheerio.load(html);
-            
-            // detalles
-            $('#feature-bullets').find('.a-list-item').each(function(i,elem){
-                if($(this).text().trim()!==undefined)
-                    detalle += " "+$(this).text().trim().replace('undefined','')
-            })
-            // imagenes        
-            var img = $('#landingImage').attr('data-old-hires')
-            tod = {str,detalle,img}
-            if(tod.detalle!=undefined && tod.img!=""){
-                /* insertando a la base de datos */
-                insertar_db(tod)
+/* halando datos url */
+request(url, function(error, r, html){
+    const arr = JSON.parse(html)
+    var det = []
+    if(r.statusCode==200){
+        art=arr.lista
+        for (var index = 0; index < art.length; index++) {
+            var element = art[index];
+            det = "https://www.amazon.com/dp/"+element.titulo.detalle.split('/')[4]
+ 
+           //console.log(myobj)
+           var myobj = {
+                hash: element.hash,
+                titulo: element.titulo.t,
+                des: det,
+                precio: element.precio
+            }
+            if(myobj.titulo!=undefined && myobj.img!=""){
+                console.log('paso la condicion')
+                insertardb(myobj)
             }
         }
-    })
-}
+    }
+})
 
-function insertar_db(tod){
-        MongoClient.connect(con,{ useNewUrlParser: true }, function(err, db) {
+function insertardb(myobj){
+    MongoClient.connect(con,{ useNewUrlParser: true }, function(err, db) {
         var dbo = db.db("linuxcoro");
-        dbo.collection("articles").findOne({'hash':tod.str.hash}, function(err, result) {
+        dbo.collection("articles").findOne({'hash':myobj.hash}, function(err, result) {
             if (!result) {
-                var myobj = {
-                    hash: tod.str.hash,
-                    titulo_ingles: tod.str.titulo,
-                    detalle_ingles: tod.detalle,
-                    precio: tod.str.precio,
-                    imagen: tod.img
-                }
                 dbo.collection("articles").insertOne(myobj, function(err, res) {
                     if (err) throw err;
                     console.log("1 document inserted");
@@ -56,26 +49,4 @@ function insertar_db(tod){
             }
         }) 
     })
-}
-
-/* halando datos url */
-request(url, function(error, r, html){
-    const arr = JSON.parse(html)
-    var det = []
-    if(r.statusCode==200){
-        art=arr.lista
-        for (var index = 0; index < art.length; index++) {
-            var element = art[index];
-            det = "https://www.amazon.com/dp/"+element.titulo.detalle.split('/')[4]
- 
-            var myobj = {
-                hash: element.hash,
-                titulo: element.titulo.t,
-                des: det,
-                precio: element.precio
-            };
-            //detalle(myobj)
-            console.log(myobj)
-        }
-    }
-})
+} 
