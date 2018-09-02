9a4bf59e47b45a2116b48c45f5e6b16b625edc8a
linuxcoro <idrogo.edixon@gmail.com>
2018-09-02 04:52:46 -0400

    Update ima.py

--- ima.py
+++ ima.py
@@ -13,4 +13,4 @@ if r.status_code == 200:
 			filename = url.split('/')[-1]
 			r = requests.get(url, allow_redirects=True)
 			open('public/img/'+filename, 'wb').write(r.content)
-			print 'imagen guardada'
+			print ('imagen guardada')
