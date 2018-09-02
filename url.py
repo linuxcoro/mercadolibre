99464f1f66c93fef31f47660eac939eaa24f40ab
linuxcoro <idrogo.edixon@gmail.com>
2018-09-02 04:26:19 -0400

    Update url.py

--- url.py
+++ url.py
@@ -4,9 +4,8 @@ req = 'https://api.mlab.com/api/1/databases/linuxcoro/collections/articles?apiKe
 r = requests.get(req)
 if r.status_code == 200:
   decoded = r.json()
-    for x in decoded:
-      if len(x['descripcion']) == 0:
-        os.system('node insertar2.js ' + x['des'].split('/')[-1] + ' ' + x['hash'])
-        os.system('python3 ima.py'+ ' ' + x['hash'])
-        #os.system('python ima.py'+ ' ' + x['hash'])
-			
+  for x in decoded:
+    if len(x['descripcion']) == 0:
+      os.system('node insertar2.js ' + x['des'].split('/')[-1] + ' ' + x['hash'])
+      os.system('python3 ima.py'+ ' ' + x['hash'])
+      #os.system('python ima.py'+ ' ' + x['hash'])
