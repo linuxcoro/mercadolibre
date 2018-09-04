2a527a68cbb1221549665a6e96b046fd6e8d7d92
Idrogo Mora Edixon Jhoan <eidrog01@cantv.com.ve>
2018-09-04 10:16:54 -0400

    corrigiendo enlace rotos de las imagenes

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ app.get('/json', function(req, res) {
 });
 
 app.get('/', function(req, res){
-    var url2 = 'https://api.mlab.com/api/1/databases/linuxcoro/collections/articles?apiKey=DSgbEQwpXRchIpWtCLjgEH-h83rECC4i'
+    var url2 = 'https://api.mlab.com/api/1/databases/linuxcoro/collections/articles?q={"status":"1"}&apiKey=DSgbEQwpXRchIpWtCLjgEH-h83rECC4i'
     request(url2, function(error, r, html){
         const arr = JSON.parse(html)
         res.render('index', {'json': arr});
