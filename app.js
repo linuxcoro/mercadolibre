9128eec39b998f92f05af0186f4dffa5aa4aaf51
Idrogo Mora Edixon Jhoan <eidrog01@cantv.com.ve>
2018-09-19 10:51:31 -0400

    agregando precios bolivares bitcoin

--- app.js
+++ app.js
@@ -42,37 +42,51 @@ app.get('/', function(req, res){
     https://localbitcoins.com/buy-bitcoins-online/amazon-gift-card-code/.json    
  */
     let urls = [
-        'https://localbitcoins.com/sell-bitcoins-online/amazon-gift-card-code/.json',
+        'https://localbitcoins.com/buy-bitcoins-online/VES/.json',
         'https://api.mlab.com/api/1/databases/linuxcoro/collections/articles?q={"status":"1"}&apiKey=DSgbEQwpXRchIpWtCLjgEH-h83rECC4i'
     ];
       
     let promises = urls.map(url => fetchJSON(url));      
-    Promise.all(promises)
-        .then((responses) => {
-            var ruta1 = responses[0].data.ad_list
-            var todo1=[]
-            for (var i in ruta1)
-                todo1[i] = Number(ruta1[i].data.temp_price)
-
-            var result = todo1[0];
-            todo1.forEach(function(x) {
-                if (parseFloat(x) < result) result = x; // find smallest number as string instead
-            });
-            var s= responses[0].url
-
-
-            var ruta2 = responses[1]
-//            res.render('index', {'json': ruta2 , 'gift': typeof(result)});
-            res.render('index', {'json': ruta2 , 'gitf': result })
-
-            dust.helpers.ima = function (chunk, context, bodies, params) {
-                var x = dust.helpers.tap(params.x, chunk, context),
-                xx = 'https://linuxcoro.herokuapp.com/img/'+x.split('/')[4]+'.jpg';
-                return xx;
-            };
-        
-
+    Promise.all(promises).then((responses) => {
+        var ruta1 = responses[0].data.ad_list
+        var coco=[]
+        var filtro1=[]
+        var filtro2=[]
+        var ent =[]
+        ruta1.forEach(function(x,i) {
+            v = Number(ruta1[i].data.profile.trade_count.replace("+",""))
+            c = Number(ruta1[i].data.profile.feedback_score) 
+            if(v >= 500 && c == 100 && v != null){
+                ent[i] = parseInt(ruta1[i].data.temp_price)
+                coco[i]={
+                    precio : Number(ruta1[i].data.temp_price),
+                    entero: ent,
+                    ventas : v,
+                    confianza : c                        
+                }        
+            }                
         });
+ 
+        filtro1 = ent.filter(function(e) {
+            return e === 0 ? '0' : e
+        })
+        filtro2 = coco.filter(function(e) {
+            return e === 0 ? '0' : e
+        })
+        indice = Math.min.apply(null,filtro1)
+
+        ix = filtro1.indexOf(indice)
+
+        var ruta2 = responses[1]
+
+        res.render('index', {'json': ruta2 , 'gitf': filtro2[ix].precio })
+
+        dust.helpers.ima = function (chunk, context, bodies, params) {
+            var x = dust.helpers.tap(params.x, chunk, context),
+            xx = 'https://linuxcoro.herokuapp.com/img/'+x.split('/')[4]+'.jpg';
+            return xx;
+        };
+    });
 });
 
 
