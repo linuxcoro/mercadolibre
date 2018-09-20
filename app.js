857950ee2cfa4fb0213ad328289e3fec23e64bea
Idrogo Mora Edixon Jhoan <eidrog01@cantv.com.ve>
2018-09-20 13:51:04 -0400

    agregando clase para la conversion a dolares

--- app.js
+++ app.js
@@ -1,99 +1,104 @@
 var path = require('path'),
-    bodyParser = require('body-parser'),
     cons = require('consolidate'),
     dust = require('dustjs-helpers'),
-    request = require('request'),
-    axios=require('axios'),
-    cryptojs = require("crypto-js"),
     express = require('express'),
-    cheerio = require('cheerio'),
     AmazonListScraper = require('amazon-list-scraper').default,
-    fetch = require('node-fetch');
+    fetch = require('node-fetch')
     
-var app = express();
-    app.engine('dust', cons.dust);
-    app.set('view engine', 'dust');
-    app.set('views', __dirname + '/views');
-    app.use(express.static(path.join(__dirname,'public')));
-
+var app = express()
+    app.engine('dust', cons.dust)
+    app.set('view engine', 'dust')
+    app.set('views', __dirname + '/views')
+    app.use(express.static(path.join(__dirname,'public')))
 
 function fetchJSON(url) {
-    return fetch(url).then(response => response.json());
+    return fetch(url).then(response => response.json())
 }
 
 app.get('/json', function(req, res) {
-
-    const scraper = new AmazonListScraper();
+    const scraper = new AmazonListScraper()
     scraper.scrape('https://www.amazon.com/registry/wishlist/1A7GB9IL1UAK2/')
       .then(items => {
-        //res.json(items);
         res.json({ 'lista': items });
       })
       .catch(error => {
       });    
-
-
 });
 
-app.get('/', function(req, res){
-/* 
-    se tomara las api para calcular el precio en bolivare de los articulos
-    https://localbitcoins.com/sell-bitcoins-online/amazon-gift-card-code/.json
-    https://localbitcoins.com/buy-bitcoins-online/amazon-gift-card-code/.json    
- */
-    let urls = [
-        'https://localbitcoins.com/buy-bitcoins-online/VES/.json',
-        'https://api.mlab.com/api/1/databases/linuxcoro/collections/articles?q={"status":"1"}&apiKey=DSgbEQwpXRchIpWtCLjgEH-h83rECC4i'
-    ];
-      
-    let promises = urls.map(url => fetchJSON(url));      
-    Promise.all(promises).then((responses) => {
-        var ruta1 = responses[0].data.ad_list
+class Salida {
+    constructor(val){
+        this.val=val
+    }
+    buscar(){
+        var val=this.val
         var coco=[]
-        var filtro1=[]
-        var filtro2=[]
         var ent =[]
-        ruta1.forEach(function(x,i) {
-            v = Number(ruta1[i].data.profile.trade_count.replace("+",""))
-            c = Number(ruta1[i].data.profile.feedback_score) 
+        val.forEach(function(x,i) {
+            var v = Number(val[i].data.profile.trade_count.replace("+",""))
+            var c = Number(val[i].data.profile.feedback_score) 
             if(v >= 500 && c == 100 && v != null){
-                ent[i] = parseInt(ruta1[i].data.temp_price)
+                ent[i] = parseInt(val[i].data.temp_price)
                 coco[i]={
-                    precio : Number(ruta1[i].data.temp_price),
+                    precio : Number(val[i].data.temp_price),
                     entero: ent,
                     ventas : v,
                     confianza : c                        
                 }        
             }                
         });
- 
+        return this.filtrar(ent,coco)
+    }
+
+    filtrar(ent,coco){
+        var filtro1=[]
+        var filtro2=[]
+    
         filtro1 = ent.filter(function(e) {
             return e === 0 ? '0' : e
         })
         filtro2 = coco.filter(function(e) {
             return e === 0 ? '0' : e
         })
-        indice = Math.min.apply(null,filtro1)
+        var indice = Math.min.apply(null,filtro1)
+        var ix = filtro1.indexOf(indice)
+        return filtro2[ix].precio
+    }
+} 
 
-        ix = filtro1.indexOf(indice)
-
-        var ruta2 = responses[1]
-
-        res.render('index', {'json': ruta2 , 'gitf': filtro2[ix].precio })
+app.get('/', function(req, res){
+    let urls = [
+        'https://localbitcoins.com/buy-bitcoins-online/USD/.json',
+        'https://localbitcoins.com/buy-bitcoins-online/VES/.json',
+        'https://api.mlab.com/api/1/databases/linuxcoro/collections/articles?q={"status":"1"}&apiKey=DSgbEQwpXRchIpWtCLjgEH-h83rECC4i'
+    ];
+    let promises = urls.map(url => fetchJSON(url));
+    Promise.all(promises).then((responses) => {
+        var ruta1 = responses[0].data.ad_list
+        var ruta2 = responses[1].data.ad_list
+        var ruta3 = responses[2]
+        var obj1 = new Salida(ruta1)
+        var dolar = obj1.buscar()
+        var obj2 = new Salida(ruta2)
+        var bolivar = obj2.buscar()
+        var convertir = (1/dolar)*bolivar
+        res.render('index', {'json': ruta3 , 'gitf': convertir })
 
         dust.helpers.ima = function (chunk, context, bodies, params) {
             var x = dust.helpers.tap(params.x, chunk, context),
-            xx = 'https://linuxcoro.herokuapp.com/img/'+x.split('/')[4]+'.jpg';
-            return xx;
-        };
-    });
-});
+            xx = 'https://linuxcoro.herokuapp.com/img/'+x.split('/')[4]+'.jpg'
+            return xx
+        }
 
-
-
+        dust.helpers.sum = function (chunk, context, bodies, params) {
+            var x = dust.helpers.tap(params.x, chunk, context)
+            var y = dust.helpers.tap(params.y, chunk, context)
+            sum=(x*y)
+            return parseInt(sum)
+        }
+    })
+})
 
 app.listen(process.env.PORT || 8080);
-
 if (process.env.PORT === undefined) {
-  console.log('Running at: http://localhost:8080');
+  console.log('Running at: http://localhost:8080')
 }
