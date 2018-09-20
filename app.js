144c1db35a851f25dc647d1a4955dbed4f10448d
Idrogo Mora Edixon Jhoan <eidrog01@cantv.com.ve>
2018-09-20 14:51:04 -0400

    cambiando a let

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const path = require('path'),
-    cons = require('consolidate'),
-    dust = require('dustjs-helpers'),
-    express = require('express'),
-    AmazonListScraper = require('amazon-list-scraper').default,
-    fetch = require('node-fetch')
+      cons = require('consolidate'),
+      dust = require('dustjs-helpers'),
+      express = require('express'),
+      AmazonListScraper = require('amazon-list-scraper').default,
+      fetch = require('node-fetch')
     
 const app = express()
-    app.engine('dust', cons.dust)
-    app.set('view engine', 'dust')
-    app.set('views', __dirname + '/views')
-    app.use(express.static(path.join(__dirname,'public')))
+      app.engine('dust', cons.dust)
+      app.set('view engine', 'dust')
+      app.set('views', __dirname + '/views')
+      app.use(express.static(path.join(__dirname,'public')))
 
 function fetchJSON(url) {
     return fetch(url).then(response => response.json())
@@ -30,12 +30,12 @@ class Salida {
         this.val=val
     }
     buscar(){
-        var val=this.val
-        var coco=[]
-        var ent =[]
+        let val=this.val
+        let coco=[]
+        let ent =[]
         val.forEach(function(x,i) {
-            var v = Number(val[i].data.profile.trade_count.replace("+",""))
-            var c = Number(val[i].data.profile.feedback_score) 
+            let v = Number(val[i].data.profile.trade_count.replace("+",""))
+            let c = Number(val[i].data.profile.feedback_score) 
             if(v >= 500 && c == 100 && v != null){
                 ent[i] = parseInt(val[i].data.temp_price)
                 coco[i]={
@@ -50,8 +50,8 @@ class Salida {
     }
 
     filtrar(ent,coco){
-        var filtro1=[]
-        var filtro2=[]
+        let filtro1=[]
+        let filtro2=[]
     
         filtro1 = ent.filter(function(e) {
             return e === 0 ? '0' : e
@@ -59,8 +59,8 @@ class Salida {
         filtro2 = coco.filter(function(e) {
             return e === 0 ? '0' : e
         })
-        var indice = Math.min.apply(null,filtro1)
-        var ix = filtro1.indexOf(indice)
+        let indice = Math.min.apply(null,filtro1)
+        let ix = filtro1.indexOf(indice)
         return filtro2[ix].precio
     }
 } 
@@ -73,25 +73,25 @@ app.get('/', function(req, res){
     ];
     let promises = urls.map(url => fetchJSON(url));
     Promise.all(promises).then((responses) => {
-        var ruta1 = responses[0].data.ad_list
-        var ruta2 = responses[1].data.ad_list
-        var ruta3 = responses[2]
-        var obj1 = new Salida(ruta1)
-        var dolar = obj1.buscar()
-        var obj2 = new Salida(ruta2)
-        var bolivar = obj2.buscar()
-        var convertir = (1/dolar)*bolivar
+        let ruta1 = responses[0].data.ad_list
+        let ruta2 = responses[1].data.ad_list
+        let ruta3 = responses[2]
+        let obj1 = new Salida(ruta1)
+        let dolar = obj1.buscar()
+        let obj2 = new Salida(ruta2)
+        let bolivar = obj2.buscar()
+        let convertir = (1/dolar)*bolivar
         res.render('index', {'json': ruta3 , 'gitf': convertir })
 
         dust.helpers.ima = function (chunk, context, bodies, params) {
-            var x = dust.helpers.tap(params.x, chunk, context),
+            let x = dust.helpers.tap(params.x, chunk, context),
             xx = 'https://linuxcoro.herokuapp.com/img/'+x.split('/')[4]+'.jpg'
             return xx
         }
 
         dust.helpers.sum = function (chunk, context, bodies, params) {
-            var x = dust.helpers.tap(params.x, chunk, context)
-            var y = dust.helpers.tap(params.y, chunk, context)
+            let x = dust.helpers.tap(params.x, chunk, context)
+            let y = dust.helpers.tap(params.y, chunk, context)
             sum=(x*y)
             return parseInt(sum)
         }
